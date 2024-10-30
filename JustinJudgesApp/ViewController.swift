dab840f8971c05c9a313c12e21696da7611f9497
Justin Weber <justinweber805@outlook.com>
2024-10-30 07:52:43 -0500

    Initial Commit

---
+++ JustinJudgesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JustinJudgesApp
+//
+//  Created by JUSTIN WEBER on 10/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
